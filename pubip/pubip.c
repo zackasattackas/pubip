6f22f4eae143526cafdaa9d8e321b3253468518d
Zack Bolin <bolinzachary@gmail.com>
2018-07-22 21:00:59 -0700

    Fixed a bug.

--- pubip/pubip.c
+++ pubip/pubip.c
@@ -75,7 +75,7 @@ LPSTR GetErrorMessage(DWORD error)
 
 	FormatMessage(
 		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE,
-		GetModuleHandle(TEXT("wininet.dll")),
+		GetModuleHandle("wininet.dll"),
 		error,
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		(LPSTR)&text,
